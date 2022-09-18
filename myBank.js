3d1c9e1c494f467ab9229fe82e0739babd7b0978
Osamsho <tajshyam21@gmail.com>
2022-09-19 02:22:10 +0600

    Bank and vault

---
+++ myBank.js
@@ -0,0 +1,20 @@
+document.getElementById('login-submit').addEventListener('click',function(){
+
+    //get user email
+    const emailField = document.getElementById('user-email')
+    const userEmail = emailField.value;
+    
+    // get user password
+
+    const passwordField = document.getElementById('user-password')
+    const password = passwordField.value;
+    
+// condtion
+
+if(userEmail == 'shyam@gmail.com' && password == 'shyam'){
+    window.location.href = 'vault.html';
+}
+else{
+    alert('Khankir put!!!!');
+}
+})
